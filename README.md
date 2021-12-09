ba90c20745fd0b42854bd95af2d6b009fd4995b0
Tired Sosha <42004251+tiredsosha@users.noreply.github.com>
2021-12-09 21:59:10 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+server + win cliend
---
+++ raspberry/main.py
@@ -0,0 +1,26 @@
+import yaml
+import logging
+
+from wakeonlan import send_magic_packet
+
+logging.basicConfig(
+    filename="WOL.log",
+    level=logging.INFO,
+    format="%(asctime)s:%(levelname)s:%(message)s",
+)
+
+
+def main():
+    with open('config/pc.yaml') as pc:
+        pcs = yaml.safe_load(pc)
+
+    def wake():
+        for mac in pcs.values():
+            send_magic_packet(mac)
+
+    logging.info("Sending packages to wake on lan\n")
+    wake()
+
+
+if __name__ == '__main__':
+    main()
